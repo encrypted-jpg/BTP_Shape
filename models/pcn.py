6a557ae19c40fe050cc4b924436cb523a15f10c2
encrypted_jpg <vallakatlaaditya@gmail.com>
2023-03-14 11:44:32 +0530

    Added Out

--- models/pcn.py
+++ models/pcn.py
@@ -94,3 +94,19 @@ class PCN(nn.Module):
         feature = self.second_conv(feature)                                                  # (B, 1024, N)
         feature_global = torch.max(feature,dim=2,keepdim=False)[0]                           # (B, 1024)
         return feature_global
+    
+    def get_out(self, feat):
+        B, _ = feat.shape
+        coarse = self.mlp(feat).reshape(-1, self.num_coarse, 3)                    # (B, num_coarse, 3), coarse point cloud
+        point_feat = coarse.unsqueeze(2).expand(-1, -1, self.grid_size ** 2, -1)             # (B, num_coarse, S, 3)
+        point_feat = point_feat.reshape(-1, self.num_dense, 3).transpose(2, 1)               # (B, 3, num_fine)
+
+        seed = self.folding_seed.unsqueeze(2).expand(B, -1, self.num_coarse, -1)             # (B, 2, num_coarse, S)
+        seed = seed.reshape(B, -1, self.num_dense)                                           # (B, 2, num_fine)
+
+        feature_global = feat.unsqueeze(2).expand(-1, -1, self.num_dense)          # (B, 1024, num_fine)
+        feat = torch.cat([feature_global, seed, point_feat], dim=1)                          # (B, 1024+2+3, num_fine)
+    
+        fine = self.final_conv(feat) + point_feat                                            # (B, 3, num_fine), fine point cloud
+
+        return coarse.contiguous(), fine.transpose(1, 2).contiguous()
